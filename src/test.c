99b38d239bf812ab2da6117940eca458da4f0079
Bogdan Lezhepekov <blezhepekov@suse.de>
2022-07-13 18:52:30 +0300

    Print uint properly

--- src/test.c
+++ src/test.c
@@ -5,6 +5,39 @@ static char horrible_buffer[4096];
 static uint32_t bp;
 
 
+char *str_reverse_in_place(char *str, int len)
+{
+    char *p1 = str;
+    char *p2 = str + len - 1;
+
+    while (p1 < p2) {
+        char tmp = *p1;
+        *p1++ = *p2;
+        *p2-- = tmp;
+    }
+
+    return str;
+}
+
+
+void print_uint(unsigned int digit) {
+    char digits[] = "0123456789";
+    char buf[25];
+    unsigned int c = 0;
+
+    while (digit > 0)
+    {
+        unsigned int m = digit % 10;
+        buf[c] = digits[m];
+        digit /= 10;
+        c++;
+    }
+    str_reverse_in_place(buf, c);
+    buf[c+1] = '\0';
+    print_uart(UART0, buf);
+}
+
+
 void echo_console(void) {
     /* do echo */
     char c = UART0->DR;
@@ -22,14 +55,17 @@ void echo_console(void) {
 }
 
 void __attribute__((interrupt)) uart0_irq_handler() {
+    char buf[100];
+
     echo_console();
     
-    UART0->DR = readl(CONFIG_SYS_TIMER_COUNTER);
+    unsigned int time = get_timer_masked();
+    print_uint(time);
+    // UART0->DR = time;
 
     VIC_INTCLEAR = 0;
 }
- 
-/* all other handlers are infinite loops */
+
 void __attribute__((interrupt)) undef_handler(void) { for(;;); }
 void __attribute__((interrupt)) swi_handler(void) { for(;;); }
 void __attribute__((interrupt)) prefetch_abort_handler(void) { for(;;); }
@@ -47,40 +83,6 @@ while(vectors_src < &vectors_end)
     *vectors_dst++ = *vectors_src++;
 }
 
-
-// int timer_init (void)
-// {
-// 	unsigned long tmr_ctrl_val;
-
-// 	/* 1st disable the Timer */
-// 	tmr_ctrl_val = *(volatile unsigned long *)(CONFIG_SYS_TIMERBASE + 8);
-// 	tmr_ctrl_val &= ~TIMER_ENABLE;
-// 	*(volatile unsigned long *)(CONFIG_SYS_TIMERBASE + 8) = tmr_ctrl_val;
-
-// 	/*
-// 	 * The Timer Control Register has one Undefined/Shouldn't Use Bit
-// 	 * So we should do read/modify/write Operation
-// 	 */
-
-// 	/*
-// 	 * Timer Mode : Free Running
-// 	 * Interrupt : Disabled
-// 	 * Prescale : 8 Stage, Clk/256
-// 	 * Tmr Siz : 16 Bit Counter
-// 	 * Tmr in Wrapping Mode
-// 	 */
-// 	tmr_ctrl_val = *(volatile unsigned long *)(CONFIG_SYS_TIMERBASE + 8);
-// 	tmr_ctrl_val &= ~(TIMER_MODE_MSK | TIMER_INT_EN | TIMER_PRS_MSK | TIMER_SIZE_MSK | TIMER_ONE_SHT );
-// 	tmr_ctrl_val |= (TIMER_ENABLE | TIMER_PRS_8S);
-
-// 	*(volatile unsigned long *)(CONFIG_SYS_TIMERBASE + 8) = tmr_ctrl_val;
-
-//     *(volatile unsigned int *)(VERSATILE_SCTL_BASE) |=
-// 	  ((VERSATILE_TIMCLK << VERSATILE_TIMER1_EnSel) | (VERSATILE_TIMCLK << VERSATILE_TIMER2_EnSel) |
-// 	   (VERSATILE_TIMCLK << VERSATILE_TIMER3_EnSel) | (VERSATILE_TIMCLK << VERSATILE_TIMER4_EnSel));
-
-// 	return 0;
-// }
 
 void core_main() {
 
