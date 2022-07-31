098102c5f5e1e46f6e1c2686af1e3c5449836fa0
Bogdan Lezhepekov <blezhepekov@suse.de>
2022-07-31 12:34:02 +0300

    Implement debug module

---
+++ core/debug.c
@@ -0,0 +1,14 @@
+#include "debug.h"
+
+
+
+void debug(const char *message, unsigned level)
+{
+    unsigned enabled = 0;
+    #ifdef DEBUG_LEVEL
+    if (level >= DEBUG_LEVEL)
+        enabled = 1;
+    #endif
+    if (enabled)
+        print_uart(message);
+}
---
+++ core/debug.h
@@ -0,0 +1,6 @@
+#pragma once
+
+
+#define DEBUG_WARN  (3)
+#define DEBUG_TRACE (2)
+#define DEBUG_ALL (1)
