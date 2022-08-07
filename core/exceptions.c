5d6ea208d070c7e969241d600c7027828abc3c23
Bogdan Lezhepekov <blezhepekov@suse.de>
2022-08-08 00:57:41 +0300

    Align interrupt naming with ARM docs
    URL: https://developer.arm.com/documentation/100933/0100/AArch64-exception-vector-table

--- core/exceptions.c
+++ core/exceptions.c
@@ -1,106 +1,106 @@
 #include "exception.h"
 
 
-void do_bad_sync_el1(void) 
+void curr_el_sp0_sync(void) 
 { 
-    printf("do_bad_sync interrupt happened!\n");
+    printf("curr_el_sp0_sync interrupt happened!\n");
     for(;;);
  }
 
-void do_bad_irq_el1(void) 
+void curr_el_sp0_irq(void) 
 { 
-    printf("do_bad_irq_el1 interrupt happened!\n");
+    printf("curr_el_sp0_irq interrupt happened!\n");
     for(;;);
 }
 
-void do_bad_fiq_el1(void) 
+void curr_el_sp0_fiq(void) 
 { 
-    printf("do_bad_fiq interrupt happened!\n");
+    printf("curr_el_sp0_fiq interrupt happened!\n");
     for(;;);
 }
 
-void do_bad_error_el1(void) 
+void curr_el_sp0_serror(void) 
 { 
-    printf("do_bad_error interrupt happened!\n");
+    printf("curr_el_sp0_serror interrupt happened!\n");
     for(;;);
 }
 
-void do_sync_el1(void) 
+void curr_el_spx_sync(void) 
 { 
-    printf("do_sync interrupt happened!\n");
+    printf("curr_el_spx_sync interrupt happened!\n");
     for(;;);
 }
 
-void do_fiq_el1(void) 
+void curr_el_spx_fiq(void) 
 {  
-    printf("do_fiq interrupt happened!\n");
+    printf("curr_el_spx_fiq interrupt happened!\n");
     for(;;);
 }
 
-void do_error_el1(void) 
+void curr_el_spx_serror(void) 
 { 
-    printf("do_error interrupt happened!\n");
+    printf("curr_el_spx_serror interrupt happened!\n");
     for(;;);
 }
 
-void do_irq_el1(void) 
+void curr_el_spx_irq(void) 
 { 
     general_purpose_irq();
 }
 
 
-void do_bad_sync_el0(void) 
+void lower_el_aarch32_sync(void) 
 { 
-    printf("do_bad_sync interrupt happened!\n");
+    printf("lower_el_aarch32_sync interrupt happened!\n");
     for(;;);
  }
 
-void do_bad_irq_el0(void) 
+void lower_el_aarch64_irq(void) 
 { 
     general_purpose_irq();
 }
 
-void do_bad_fiq_el0(void) 
+void lower_el_aarch64_fiq(void) 
 { 
-    printf("do_bad_fiq interrupt happened!\n");
+    printf("lower_el_aarch64_fiq interrupt happened!\n");
     for(;;);
 }
 
-void do_bad_error_el0(void) 
+void lower_el_aarch64_serror(void) 
 { 
-    printf("do_bad_error interrupt happened!\n");
+    printf("lower_el_aarch64_serror interrupt happened!\n");
     for(;;);
 }
 
-void do_sync_el0()
+void lower_el_aarch64_sync()
 {
     register void * syndrome asm("x25");
     asm ("" : "=r"(syndrome));
 
     if ((syndrome && INSTRUCTION_TRAPPED) == INSTRUCTION_TRAPPED)
     {
-        printf("do_sync_el0: 32-bit instruction trapped\n");
+        printf("lower_el_aarch64_sync: 32-bit instruction trapped\n");
     } else {
-        printf("do_sync_el0: syndrome exception register isn't INSTRUCTION_TRAPPED: %u\n");
+        printf("lower_el_aarch64_sync: syndrome exception register isn't INSTRUCTION_TRAPPED: %u\n");
         for(;;);
     }   
 }
 
-void do_fiq_el0(void) 
+void lower_el_aarch32_fiq(void) 
 {  
-    printf("do_fiq interrupt happened!\n");
+    printf("lower_el_aarch32_fiq interrupt happened!\n");
     for(;;);
 }
 
-void do_error_el0(void) 
+void lower_el_aarch32_serror(void) 
 { 
-    printf("do_error interrupt happened!\n");
+    printf("lower_el_aarch32_serror interrupt happened!\n");
     for(;;);
 }
 
-void do_irq_el0(void) 
+void lower_el_aarch32_irq(void) 
 { 
-    printf("do_irq_el0 interrupt happened!\n");
+    printf("lower_el_aarch32_irq interrupt happened!\n");
     for(;;);
 }
 
