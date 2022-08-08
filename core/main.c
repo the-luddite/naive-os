729a3f8cca0175150c275e623d16642cdb277dad
Bogdan Lezhepekov <blezhepekov@suse.de>
2022-08-08 14:22:35 +0300

    Cleanup main: move test to test.c

--- core/main.c
+++ core/main.c
@@ -3,55 +3,6 @@
 #include "process.h"
 
 
-void user_process1(char *array)
-{
-	char buf[2] = {0};
-	while (1){
-		for (int i = 0; i < 5; i++){
-			buf[0] = array[i];
-			call_sys_write(buf);
-			delay(100000);
-		}
-	}
-}
-
-void user_process(){
-	char buf[30] = {0};
-	tfp_sprintf(buf, "User process started\n\r");
-	call_sys_write(buf);
-	// printf("User process EL %d\r\n", get_current_el());
-	unsigned long stack = call_sys_malloc();
-	if (stack < 0) {
-		printf("Error while allocating stack for process 1\n\r");
-		return;
-	}
-	int err = call_sys_clone((unsigned long)&user_process1, (unsigned long)"12345", stack);
-	if (err < 0){
-		printf("Error while clonning process 1\n\r");
-		return;
-	} 
-	stack = call_sys_malloc();
-	if (stack < 0) {
-		printf("Error while allocating stack for process 1\n\r");
-		return;
-	}
-	err = call_sys_clone((unsigned long)&user_process1, (unsigned long)"abcd", stack);
-	if (err < 0){
-		printf("Error while clonning process 2\n\r");
-		return;
-	} 
-	call_sys_exit();
-}
-
-void kernel_process(){
-	printf("Kernel process started. EL %d\r\n", get_current_el());
-	int err = move_to_user_mode((unsigned long)&user_process);
-	if (err < 0){
-		printf("Error while moving process to user mode\n\r");
-	} 
-}
-
-
 void core_main() 
 {
     debug("entered core_main\n", DEBUG_TRACE);
@@ -66,17 +17,10 @@ void core_main()
 
 	enable_irq();
 
-// #ifdef TEST_SCHED
-// 	test_sched_core_thread();
-// #endif
-	int res = copy_process(PF_KTHREAD, (unsigned long)&kernel_process, 0, 0);
-	if (res < 0) {
-		printf("error while starting kernel process");
-		return;
-	}
-
+#ifdef TEST_SCHED
+	test_sched_core_thread();
+#endif
 	
-
 	while(1) {
 		// wfi();	/* Wait for Interrupt */
 		schedule();
--- core/tests.c
+++ core/tests.c
@@ -2,38 +2,64 @@
 #include "scheduler.h"
 
 
-// void process(char *array)
-// {
-// 	while (1){
-// 		for (volatile int i = 0; i < 5; i++){
-// 			put_uart(array[i]);
-// 			delay(10000000);
-// 		}
-// 	}
-// }
+void user_process1(char *array)
+{
+	char buf[2] = {0};
+	while (1){
+		for (int i = 0; i < 5; i++){
+			buf[0] = array[i];
+			call_sys_write(buf);
+			delay(100000);
+		}
+	}
+}
 
-// void kernel_process(){
-// 	printf("Kernel process started. EL %d\r\n", get_current_el());
-// 	int err = move_to_user_mode((unsigned long)&process);
-// 	if (err < 0){
-// 		printf("Error while moving process to user mode\n\r");
-// 	} 
-// }
+void user_process(){
+	char buf[30] = {0};
+	tfp_sprintf(buf, "User process started\n\r");
+	call_sys_write(buf);
+	// printf("User process EL %d\r\n", get_current_el());
+	unsigned long stack = call_sys_malloc();
+	if (stack < 0) {
+		printf("Error while allocating stack for process 1\n\r");
+		return;
+	}
+	int err = call_sys_clone((unsigned long)&user_process1, (unsigned long)"12345", stack);
+	if (err < 0){
+		printf("Error while clonning process 1\n\r");
+		return;
+	} 
+	stack = call_sys_malloc();
+	if (stack < 0) {
+		printf("Error while allocating stack for process 1\n\r");
+		return;
+	}
+	err = call_sys_clone((unsigned long)&user_process1, (unsigned long)"abcd", stack);
+	if (err < 0){
+		printf("Error while clonning process 2\n\r");
+		return;
+	} 
+	call_sys_exit();
+}
 
 
-// void test_sched_core_thread()
-// {
-//     printf("test_sched_core_thread test started\n");
+void kernel_process(){
+	printf("Kernel process started. EL %d\r\n", get_current_el());
+	int err = move_to_user_mode((unsigned long)&user_process);
+	if (err < 0){
+		printf("Error while moving process to user mode\n\r");
+	} 
+}
 
-//     int res = copy_process(PF_KTHREAD, (unsigned long)&kernel_process, (unsigned long)"12345", 0);
-// 	if (res < 0) {
-// 		printf("error while starting process 1\n");
-// 		return;
-// 	}
-// 	res = copy_process(PF_KTHREAD, (unsigned long)&kernel_process, (unsigned long)"abcde", 0);
-// 	if (res < 0) {
-// 		printf("error while starting process 2\n");
-// 		return;
-//     }
-// 	return;
-// }
+
+void test_sched_core_thread()
+{
+    printf("test_sched_core_thread test started\n");
+
+    int res = copy_process(PF_KTHREAD, (unsigned long)&kernel_process, 0, 0);
+	if (res < 0) {
+		printf("error while starting kernel process");
+		return;
+	}
+	return;
+}
