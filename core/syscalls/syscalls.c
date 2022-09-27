0fde12e3f3c42cdcc7215a78058efc2d16b856c0
Bogdan Lezhepekov <blezhepekov@suse.de>
2022-09-27 21:52:13 +0300

    syscalls: new syscalls, align argument types

--- core/syscalls/syscalls.c
+++ core/syscalls/syscalls.c
@@ -12,7 +12,7 @@ int do_sys_clone(unsigned long fn, unsigned long arg, unsigned long stack)
 	return copy_process(0, 0, 0, stack);
 }
 
-unsigned long do_sys_malloc(u64 size)
+unsigned long do_sys_malloc(unsigned long size)
 {
 	unsigned long addr = kmalloc(size);
 	if (!addr) {
@@ -26,14 +26,19 @@ void do_sys_exit()
 	exit_process();
 }
 
-void do_sys_open(u8 *path)
+void do_sys_open(char *path)
 {
 	exit_process();
 }
 
-void do_sys_read()
+void do_sys_read(unsigned *buf)
 {
-	exit_process();
+	uard_read_buffer(buf);
 }
 
-void * const sys_call_table[] = {do_sys_write, do_sys_malloc, do_sys_clone, do_sys_exit, do_sys_open, do_sys_read};
+void do_sys_print(unsigned *buf)
+{
+	printf(buf);
+}
+
+void * const sys_call_table[] = {do_sys_write, do_sys_malloc, do_sys_clone, do_sys_exit, do_sys_open, do_sys_read, do_sys_print};
--- core/syscalls/syscalls.h
+++ core/syscalls/syscalls.h
@@ -1,15 +1,23 @@
 #pragma once
 
-#define __NR_syscalls	    4
 
-#define SYS_WRITE_NUMBER    0 		// syscal numbers 
-#define SYS_MALLOC_NUMBER   1 	
-#define SYS_CLONE_NUMBER    2 	
-#define SYS_EXIT_NUMBER     3 
+#define __NR_syscalls	    7
+
+#define SYS_WRITE_NUMBER    0
+#define SYS_MALLOC_NUMBER   1
+#define SYS_CLONE_NUMBER    2
+#define SYS_EXIT_NUMBER     3
+#define SYS_EXIT_OPEN       4
+#define SYS_EXIT_READ       5
+#define SYS_EXIT_PRINT      6
 
 #ifndef __ASSEMBLER__
 void call_sys_write(char * buf);
 int call_sys_clone(unsigned long fn, unsigned long arg, unsigned long stack);
 unsigned long call_sys_malloc();
 void call_sys_exit();
+void do_sys_open(char *path);
+void do_sys_read(unsigned *buf);
+void do_sys_print(unsigned *buf);
+
 #endif
